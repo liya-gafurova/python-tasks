ea29bdd94b380c0f7e05bc66769921a73d221082
liya-gafurova <gali4086555@gmail.com>
2021-10-23 23:11:28 +0600

    threads

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # python-tasks
 python-tasks
+
+
+# Resources
+https://habr.com/ru/company/pvs-studio/blog/584962/
+https://learn.javascript.ru/
+https://www.youtube.com/watch?v=PsLzEAsphbM&list=PLrCZzMib1e9pg7ZLIOhmGSlmkMf8yEOLZ&ab_channel=VKTeam
+https://www.youtube.com/playlist?list=PLrCZzMib1e9qZwq95WVmGB-acnot5ka4a
--- threads.py
+++ threads.py
@@ -5,25 +5,75 @@ libraries might overcome this limitation). If you want your application to make 
 use of the computational resources of multi-core machines, you are advised to use
 multiprocessing or concurrent.futures.ProcessPoolExecutor. However, threading is still
 an appropriate model if you want to run multiple I/O-bound tasks simultaneously.
+
+Стоит ли преодолевать связанные c GIL сложности и тратить время на реализацию многопоточности?
+Вот примеры ситуаций, когда многопоточность несёт с собой больше плюсов, чем минусов.
+
+-Для длительных и несвязанных друг с другом операций ввода-вывода.
+    Например, нужно обрабатывать ворох разрозненных запросов с большой задержкой на ожидание.
+    В режиме «живой очереди» это долго  — лучше распараллелить задачу.
+- Вычисления занимают более миллисекунды и вы хотите сэкономить время за счёт их параллельного выполнения.
+    Если операции укладываются в 1 мс, многопоточность не оправдает себя из-за высоких накладных расходов.
+- Число потоков не превышает количество ядер.
+    В противном случае параллельной работы всех потоков не получается и мы больше теряем, чем выигрываем.
 """
+import math
+import os
+import random
 import threading
 import time
+import datetime
 from threading import Thread
 
-
-def print_something(arg):
-    print(f'{arg} - your arg')
-    time.sleep(5)
-    print('after sleep')
-
+FILES_DIR = './files/'
+NUMBER_OF_FILES = 1000
+NUMBER_OF_LINES = 1000
+THREAD_COUNT = os.cpu_count()
 
 
-for i in range(5):
-    thread = Thread(target=print_something, args=(i,))
+def create_file(i):
+    with open(f'./files/{i}-file.txt', 'w') as new_file:
+        for line_number in range(NUMBER_OF_LINES):
+            new_file.write(str(random.randint(1, 1000000)))
+
+
+for file in os.listdir(FILES_DIR):
+    os.remove(os.path.join(FILES_DIR, file))
+
+
+# # 0:00:00.763496 - time
+# time_start = datetime.datetime.now()
+# for j in range(NUMBER_OF_FILES):
+#     create_file(j)
+#
+# time_finish = datetime.datetime.now()
+# print(f'{time_finish - time_start} - time')
+
+
+class MyThread(Thread):
+    def __init__(self, a, b):
+        super(MyThread, self).__init__()
+        self.a = a
+        self.b = b
+
+    def run(self) -> None:
+        print(f'current thread - {self.name}')
+        for i in range(self.a, self.b):
+            create_file(i)
+        print(f'finished thread - {self.name}')
+
+
+a_list = [i * math.floor(NUMBER_OF_FILES / THREAD_COUNT) for i in range(THREAD_COUNT)]
+b_list = a_list[1:]
+b_list.append(NUMBER_OF_FILES)
+assert len(a_list) == len(b_list)
+
+
+# 0:00:00.084930 - time --- не чистое время, так как считается время, как потоки запустились, но не доджилается заверщения
+time_start = datetime.datetime.now()
+for a, b in list(zip(a_list, b_list)):
+    thread = MyThread(a, b)
     thread.start()
-    print(threading.active_count())
-    thread.join()
-
-
-
+time_finish = datetime.datetime.now()
+print(f'{time_finish - time_start} - time')
 
